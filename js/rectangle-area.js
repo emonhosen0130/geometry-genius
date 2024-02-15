df770f5f769f2c67e012d3808439d76c2b76de71
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-02-15 11:17:01 +0600

    parallelogram

--- js/rectangle-area.js
+++ js/rectangle-area.js
@@ -1,3 +1,37 @@
-function calculateRectangleArea(){
-    console.log('calculate rect area')
+function calculateRectangleArea() {
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    const area = length * width;
+    console.log('area of the rectangle :', area);
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
+}
+
+function calculateParallelogramArea(){
+        const bInput = document.getElementById('parallelogram-b');
+        const bText = bInput.value;
+        const b = parseFloat(bText);
+        console.log(b);
+
+        const hInput = document.getElementById('parallelogram-h');
+        const hText = hInput.value;
+        const h = parseFloat(hText);
+        console.log(h);
+
+        const area = b * h;
+        console.log('area of the parallelogram :', area);
+
+        const parallelogramAreaSpan = document.getElementById('parallelogram-area');
+        parallelogramAreaSpan.innerText = area;
+
 }
